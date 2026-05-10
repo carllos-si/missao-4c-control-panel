e92a8a4153e16ad226270a1848f7df8c941f191b
carllos-si <carllos_si@yahoo.com.br>
2026-05-10 11:33:04 -0300

    Fix: tipagem de cookies no middleware

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -15,10 +15,10 @@ export async function updateSession(request: NextRequest) {
       getAll() {
         return request.cookies.getAll();
       },
-      setAll(cookiesToSet) {
-        cookiesToSet.forEach(({ name, value }) =>
-          request.cookies.set(name, value)
-        );
+setAll(cookiesToSet: { name: string; value: string; options: any }[]) {
+  cookiesToSet.forEach(({ name, value }) =>
+    request.cookies.set(name, value)
+  );
         supabaseResponse = NextResponse.next({ request });
         cookiesToSet.forEach(({ name, value, options }) =>
           supabaseResponse.cookies.set(name, value, options)
